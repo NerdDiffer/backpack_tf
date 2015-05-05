cadc027261d66618355345200262368d30d04985
Rafael Espinoza <rafael@rafaelespinoza.com>
2015-05-05 00:10:01 -0700

    add some tests for Prices class, spread out error message over several lines

--- lib/backpack_tf/prices.rb
+++ lib/backpack_tf/prices.rb
@@ -61,7 +61,10 @@ module BackpackTF
     end
 
     def initialize
-      msg = "This class is meant to receive the JSON response from the #{self.class.interface} interface. It holds a Hash array of prices of items, but not is meant to be instantiated. See the Item class if you are interested in an item. However, information on items should be stored in the @items property of #{self.class}"
+      msg = "This class is meant to receive the JSON response from the #{self.class.interface} interface."
+      msg << "It holds a Hash array of prices of items, but not is meant to be instantiated."
+      msg << "See the Item class if you are interested in an item."
+      msg << "However, information on items should be stored in the @items property of #{self.class}."
       raise RuntimeError, msg
     end
 
--- spec/backpack_tf/prices_spec.rb
+++ spec/backpack_tf/prices_spec.rb
@@ -9,7 +9,6 @@ module BackpackTF
       end
 
       describe 'accessing the IGetPrices interface' do
-
         context 'a successful response' do
           it 'returns the fixture and sets to @@items variable' do
             stub_http_response_with('prices.json')
@@ -30,23 +29,38 @@ module BackpackTF
             expect(Prices.response[:usd_currency]).to eq 'metal'
             expect(Prices.response[:usd_currency_index]).to eq 5002
           end
+        end
+      end
 
-          it '@@items attribute should be a Hash object' do
+      describe '::items' do
+        context 'describing the @@items attribute' do
+          it 'should be a Hash object' do
             expect(Prices.items).to be_instance_of Hash
           end
-
-          it '@@items should have this many keys' do
+          it 'should have this many keys' do
             expect(Prices.items.keys.length).to be 1661
           end
+          it 'each key should be a String' do
+            expect(Prices.items.keys).to all be_a String
+          end
+        end
+        context 'using keys to generate Item objects' do
 
-          it '@@items should have these keys' do
-            expect(Prices.items).to have_key('Kritzkrieg')
+          let(:random_key) { Prices.items.keys.sample }
+          let(:item) { Item.new random_key, Prices.items[random_key] }
+
+          it 'generates an Item object' do
+            expect(item).to be_instance_of Item
+          end
+          it 'uses the key to assign a value to the @item_name property of the Item' do
+            expect(random_key).to eq item.item_name
+          end
+          it 'passes itself to be stored in the @prices attribute of the Item object' do
+            expect(Prices.items[random_key]['prices']).to eq item.prices
           end
 
         end
-
       end
-
     end
 
     describe 'instances of Prices' do
